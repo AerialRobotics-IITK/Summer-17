36acded527a1966812167b5650b51df71e593a48
Pratyush Varshney <pratyusv@users.noreply.github.com>
2017-06-02 16:12:27 +0530

    Update motor_FINAL_WITH_distance.ino

--- motor_FINAL_WITH_distance.ino
+++ motor_FINAL_WITH_distance.ino
@@ -1,3 +1,9 @@
+/* This code transmits signals from transmitter to the 
+   motor through arduino when the distance between quad and obstacle
+   is greater than 30 cm.  If the distance comes within 30 cm then 
+   the motors stop functioning.
+   */
+
 #include<Servo.h>
 Servo quad;
 int trigPin=12;
@@ -14,9 +20,13 @@ pinMode(echoPin, INPUT);
 pinMode(ch1,INPUT);
 pinMode(servopin,OUTPUT);
 quad.attach(servopin);
+
+/* Intialize the motors with the minimum input value
 quad.writeMicroseconds(1100); 
 Serial.begin(9600);
 }
+
+
 void loop() {
 // Clears the trigPin
 digitalWrite(trigPin, LOW);
@@ -34,18 +44,18 @@ distance= duration*0.034/2;
 //Serial.print(": ");
 //Serial.println(distance);
      
-  ch1 = pulseIn(5, HIGH, 25000); // Read the pulse width of 
+ ch1 = pulseIn(5, HIGH, 25000); // Read the pulse width of the channel
 ch1= constrain(ch1,1100,1920);
  
   Serial.println(ch1);
   if(distance>30)
   {
-    quad.write(ch1);
+    quad.write(ch1); // Write output from transmitter to the motor
     
   }
 else if(distance<30)
 {
-  quad.write(1100);
+  quad.write(1100); // Stop motor as distance is less than 30 cm.
 }
 
 
